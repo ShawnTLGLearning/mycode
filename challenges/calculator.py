0d20c29b6fe0245efcf9cad8dcfd97a04e71826f
ShawnTLGLearning <shawn.charles@tlgcohort.com>
2021-04-01 21:03:31 +0000

    finished calculator

--- challenges/calculator.py
+++ challenges/calculator.py
@@ -1,13 +1,41 @@
 #!/usr/bin/env python3
-def main():
-    num1 = promptNumber("Input the First Number?")
-    num2 = promptNumber("Input the Second Number?")
-    print(promptOperation(num1,num2))
-    
+######clear screen from chad
+from subprocess import call
+from os import name as osname
+#####Chads Cool TRicks
+import sys, time
+
+def print1by1(text, delay=0.1):
+    for c in text:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print
+
+def clear(num1="",num2="",op="",result=""):
+    # check and make call for specific operating system
+    call('clear' if osname =='posix' else 'cls')
+    print("########################")
+    if num1==op:
+        print("###### CALCULATOR ######")
+    else:
+        print1by1(f"{num1} {op} {num2} = {result}\n")
+    print("########################")
+    print("########################")
+    print("## 1 ##### 2 ###### 3 ##")
+    print("########################")
+    print("## 4 ##### 5 ###### 6 ##")
+    print("########################")
+    print("## 7 ##### 8 ###### 9 ##")
+    print("########################")
+    print("########## 0 ###########")
+    print("########################")
+
 def promptNumber(string):
     while True:
         try:
-            num=float(input(string))
+            num=input(string)
+            num=float(num)
         except:
             print(num,"is not a valid number. Try Again!")
         else:
@@ -23,16 +51,33 @@ def promptOperation(num1,num2):
             "D": lambda a,b: a/b,
             "M": lambda a,b: a*b,
             "Multiply": lambda a,b: a*b,
+            "op":{
+                "A":"+",
+                "S":"-",
+                "M":"*",
+                "D":"/",
+                }
             }
     while True:
         op=input("(A)dd, (S)ubtract, (D)ivide, (M)ultiply?").upper()
         if op in operations:
             if (op == "DIVIDE" or op=="D") and num2 == 0:
-                return "DONT DESTROY THE UNIVERSE!!\nNo Dividing By ZERO!!"
-            print()
-            return operations[op](num1,num2)
+                return ["\nDONT DESTROY THE UNIVERSE!!\nNo Dividing By ZERO!!","/"]
+            return [operations[op](num1,num2),operations["op"][op[0]]]
         print(op,"not a valid operation. Try Add or Subtract")
-def updateTerminal(num1='',num2='',op=''):
 
-    
-main()
+def main():
+    while True:
+        clear()
+        num1 = promptNumber("Input the First Number?")
+        clear(num1)
+        num2 = promptNumber("Input the Second Number?")
+        clear(num1,num2)
+        answer = promptOperation(num1,num2)
+        clear(num1,num2,answer[1],answer[0])
+        state = input("Stop Calculating? \n(Y)/(N)?\n").upper()
+        if state == "Y":
+            print1by1("Thank you for using my Calculator\n")
+            break
+if __name__ == "__main__":
+    main()
